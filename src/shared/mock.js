b1c814fb56da27184e552f2ea3683171b5e386bb
Aditya <aditdualapan@gmail.com>
2025-11-29 14:58:01 +0700

    wip

--- src/shared/mock.js
+++ src/shared/mock.js
@@ -94,11 +94,6 @@ export const tracking = {
         waktu: "2025-08-25 10:12:20",
         keterangan: "Penerimaan di Loket: TANGSEL",
       },
-      { waktu: "2025-08-25 14:07:56", keterangan: "Tiba di Hub: JAKSEL" },
-      {
-        waktu: "2025-08-26 08:44:01",
-        keterangan: "Diteruskan ke Kantor Tujuan",
-      },
     ],
   },
 };
